3f277dc79a42cd62d3e91dd806bcfebd9ac570af
Bilas-Halder <bilas8642@gmail.com>
2022-05-28 01:58:13 +0600

    Dummy Members added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { ObjectID } = require('bson');
 const mongodb = require('./mongoInit.js');
 
 
+const committeeMembers = require('./routes/committeeMembers.js');
 const committee = require('./routes/committee.js');
 const userUpload = require('./routes/uploads.js');
 
@@ -18,18 +19,9 @@ const port = process.env.PORT || 5000;
 
 //middleWares
 app.use(cors());
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader(
-//         'Access-Control-Allow-Headers',
-//         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//     );
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-
-//     next();
-// });
 app.use(express.json());
 
+app.use('/committee-members', committeeMembers);
 app.use('/committee', committee);
 app.use('/upload', userUpload);
 app.use('/uploads/images', express.static('uploads/images'));
--- routes/committee.js
+++ routes/committee.js
@@ -6,14 +6,14 @@ const mongodb = require('../mongoInit.js');
 
 
 
-router.get('/:id', async (req, res) => {
+router.get('/:year', async (req, res) => {
 
     const database = mongodb.getDatabase();
     const committeeCollection = database.collection("ExecutiveCommittee");
     const committeeMembersCollection = database.collection("CommitteeMembers");
 
-    const id = req.params?.id;
-    const query = { year: id };
+    const year = req.params?.year;
+    const query = { year: year };
     const committee = await committeeCollection.findOne(query);
     const data = committee.data;
 
@@ -34,9 +34,40 @@ router.get('/:id', async (req, res) => {
         })
     );
 
-    console.log(response);
-
     res.json(response);
+});
+
+router.get('/:year/dummy', async (req, res) => {
+
+    const database = mongodb.getDatabase();
+    const committeeCollection = database.collection("ExecutiveCommittee");
+    const committeeMembersCollection = database.collection("CommitteeMembers");
+
+    const year = req.params?.year;
+    const query = { year: year };
+    const committee = await committeeCollection.findOne(query);
+    const data = committee.data;
+
+    data.sort((a, b) => a.priority < b.priority ? -1 : 1);
+
+    const response = await Promise.all(
+        data.map(async (d) => {
+            const uID = d.uID;
+            const q = { _id: ObjectID(uID) };
+            const mem = await committeeMembersCollection.findOne(q);
+
+            mem.designation = d.des;
+            mem.priority = d.priority;
+            mem.period = committee.period;
+            mem.year = committee.year;
+
+            return mem;
+        })
+    );
+
+    const dummyData = [...response, ...response, ...response, ...response, ...response, ...response, ...response, ...response, ...response, ...response,];
+
+    res.json(dummyData);
 });
 
 module.exports = router;
